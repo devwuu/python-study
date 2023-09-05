d0361dbaa71f08ab2a20ccc83f34ec545e81a8c4
devwuu <jdw0477@gmail.com>
2023-09-05 16:23:52 +0900

    add post list page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
 
 # Create your views here.
+
+# CBV (Class Based View)
+class PostList(ListView):
+    model = Post
+    template_name = 'list.html'
+    ordering = '-created_at'
