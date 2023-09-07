c868bef9f5ab51a9e1698ab78c0e1fe6e114cccd
devwuu <jdw0477@gmail.com>
2023-09-07 15:09:14 +0900

    add test case

--- blog/tests.py
+++ blog/tests.py
@@ -15,3 +15,16 @@ class TestView(TestCase):
         self.assertEqual(soup.title.text, 'Blog')
         main_area = soup.find('div', id='main-area')
         self.assertIn(' 게시글이 없습니다 ', main_area.text)
+
+    def test_post_detail(self):
+
+        # Creating test database for alias 'default'...
+        # 기본으로 테스트용 데이터베이스를 사용하도록 설정되어 있기 때문에
+        # given을 꼭 줘야 함
+
+        given = Post.objects.create(title='given', content='given content')
+        response = self.client.get('/blog/1/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        post_content = soup.find('p', id='post_content')
+        self.assertEqual(given.content, post_content.text)
