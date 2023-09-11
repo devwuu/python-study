2f7e8ba0eb6fc1acd7112b4e92c2d207d935a21b
devwuu <jdw0477@gmail.com>
2023-09-11 16:35:13 +0900

    add categorylist for detail view

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view())
+    # path variable 이름을 pk로 정해줘야 CBV를 사용할 수 있다
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,9 +8,11 @@ from .models import Post, Category
 # CBV (Class Based View)
 class PostList(ListView):
     model = Post
+    # ListView를 상속 받았기 때문에 .all()을 자동으로 실행해준다.
     template_name = 'list.html'
     ordering = '-created_at'
 
+    # 추가적으로 필요한 데이터를 가져오기 위해 오버라이딩 함
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data()
         context['categorys'] = Category.objects.all()
@@ -20,5 +22,12 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+    # DetailView를 상속 받았기 때문에 .get(pk=)를 자동으로 실행해준다.
     template_name = 'detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categorys'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
