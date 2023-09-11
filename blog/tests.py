0c22d449f278ae00847b1928a80452d3c292e81e
devwuu <jdw0477@gmail.com>
2023-09-11 16:12:07 +0900

    add category list

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from .models import Post, Category
 from django.contrib.auth.models import User
 
 
@@ -10,23 +10,38 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
         self.user_ddochi = User.objects.create_user(username='ddochi', password='1234')
+        self.category_programming = Category.objects.create(name='programming', slug='프로그래밍')
+        self.category_music = Category.objects.create(name='music', slug='음악')
+        self.post = Post.objects.create(
+            title='given',
+            content='given content',
+            category=self.category_programming,
+            author=self.user_ddochi
+        )
 
     def test_post_list(self):
         reponse = self.client.get('/blog/')
         self.assertEqual(reponse.status_code, 200)
         soup = BeautifulSoup(reponse.content, 'html.parser')
-        self.assertEqual(soup.title.text, 'Blog')
-        main_area = soup.find('div', id='main-area')
-        self.assertIn(' 게시글이 없습니다 ', main_area.text)
+        self.assertEqual(soup.title.text, ' Blog ')
+        # main_area = soup.find('div', id='main-area')
+        # self.assertIn(main_area.text, ' 게시글이 없습니다 ')
 
     def test_post_detail(self):
         # Creating test database for alias 'default'...
         # 기본으로 테스트용 데이터베이스를 사용하도록 설정되어 있기 때문에
         # given을 꼭 줘야 함
 
-        given = Post.objects.create(title='given', content='given content', author=self.user_ddochi)
+        # given = Post.objects.create(title='given', content='given content', author=self.user_ddochi)
         response = self.client.get('/blog/1/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         post_content = soup.find('p', id='post_content')
-        self.assertEqual(given.content, post_content.text)
+        self.assertEqual(self.post.content, post_content.text)
+
+    def test_category_list(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        find = soup.find('ul', id='categorys')
+        self.assertIn(self.category_programming.name, find.text)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 
 # Create your views here.
@@ -11,7 +11,14 @@ class PostList(ListView):
     template_name = 'list.html'
     ordering = '-created_at'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categorys'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
 
 class PostDetail(DetailView):
     model = Post
     template_name = 'detail.html'
+
